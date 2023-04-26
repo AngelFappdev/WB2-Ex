a7bd069169369ae938bcd7497938fc12ceb97e9b
Angel Fraser <Angelaffirms@gmail.com>
2023-04-26 10:31:42 -0400

    if statements among over things

---
+++ wrbk_exercises.js
@@ -0,0 +1,48 @@
+// sample inputs
+//let foodcost = 79.25;
+//let tax = 6.54;
+//let tip = 12.00;
+//calucations
+//let totalDue = foodcost + tax + tip;
+
+//console.log("The total due is"+ totalDue);
+
+//let assets = 20000000;
+//let debt = 10000;
+//let netWorth = assets - debt ;
+//console.log("Your networth is"+ netWorth)
+
+//let height_Rec = 20;
+//let width_Rec = 10;
+//let area_of_Rec = height_Rec * width_Rec;
+//console.log("The area of this rectangle is" + area_of_Rec)
+
+//let raidus_of_circle = 10;
+//let area_of_circle = 3.14 * raidus_of_circle ** 2;
+//console.log("The area of circle is" + area_of_circle)
+
+//let fahrenheit = 40;
+//let celsius = fahrenheit - 32 * .5556;
+//console.log ("Fahrenheit converted to celsius is " + celsius)
+
+//let income = 400000;
+//let taxes = 23/100;
+//let taxesWithheld = income * taxes
+//console.log("Taxes withheld is" + taxesWithheld)
+
+//let a = "101.1";
+//let b = "55";
+//let c = "402 Stevens"
+//let d = "Number 5";
+//a_number = Number(a)
+//b_number = Number(b)
+//c_number = Number(c)
+//d_number = Number(d)
+//console.log(a_number, b_number, c_number, d_number )
+
+let hours_worked = 45;
+let payrate =  hours_worked * 17.30; 
+if(hours_worked > 40 ){
+payrate =  payrate * 1.5;
+}
+console.log("Weekly pay check is"+ payrate )
